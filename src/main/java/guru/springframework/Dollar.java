d58814ba2d7b7ba2f14d1a5411fd696355cf1d8c
Mehdibhy0 <benhadjyahiamehdi3@gmail.com>
2023-01-11 16:34:23 +0100

    equals

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -2,12 +2,18 @@ package guru.springframework;
 
 public class Dollar {
     int amount;
-    public Dollar(int amount){
+
+    public Dollar(int amount) {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier){
-        return new Dollar(amount*multiplier);
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+    @Override
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount ==dollar.amount;
     }
 
 }
