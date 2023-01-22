2b573c8b3c71d15a5e9b75e82ded52ff2659726e
Mehdibhy0 <benhadjyahiamehdi3@gmail.com>
2023-01-22 12:02:44 +0100

    Refactoring

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,10 +1,17 @@
 package guru.springframework;
 
-public class Money {
-    protected int amount;
+public abstract class Money {
 
+    public abstract Money times(int Multiplier);
+    protected int amount;
+    public static Dollar dollar(int amount){
+        return new Dollar(amount);
+    }
+    public static Franc franc(int amount){
+        return new Franc(amount);
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount ==money.amount;
+        return amount ==money.amount && this.getClass().equals(object.getClass());
     }
 }
