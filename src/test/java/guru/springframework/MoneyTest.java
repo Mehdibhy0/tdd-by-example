74da369176104efa896ba1c661fd152df5343864
Mehdibhy0 <benhadjyahiamehdi3@gmail.com>
2023-01-23 09:33:24 +0100

    29

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -8,6 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
+    void testPlusReturnSum(){
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five,sum.augmend);
+        assertEquals(five,sum.addmend);
+    }
+    @Test
+    void testReduceSum(){
+        Expression sum= new Sum(Money.dollar(3),Money.dollar(4));
+        Bank bank= new Bank();
+        Money result =bank.reduce(sum,"USD");
+        assertEquals(Money.dollar(7),result);
+    }
+    @Test
+    void testReduceMoney(){
+        Bank bank= new Bank();
+        Money result =bank.reduce(Money.dollar(1),"USD");
+        assertEquals(Money.dollar(1),result);
+    }
+    @Test
     void TestMultiplication(){
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
@@ -29,10 +50,6 @@ public class MoneyTest {
     void TestCurrency(){
     assertEquals("USD",Money.dollar(1).currency());
     assertEquals("CHF",Money.franc(1).currency());}
-
-
-
-
 
     @Test
     void testSimpleAddition(){
